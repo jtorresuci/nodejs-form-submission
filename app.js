85690dbeddd0031e3c6f081309125d3b169e3161
Jose Torres <jtorresocc@gmail.com>
2023-02-23 22:15:29 -0800

    Updated port configuration for Heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const cors = require('cors');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 
