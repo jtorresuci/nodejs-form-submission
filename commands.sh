962ec880f21902ab58aaf8a22444805a437a4abd
Jose Torres <jtorresocc@gmail.com>
2023-02-23 22:18:05 -0800

    Updated receiving site.

--- commands.sh
+++ commands.sh
@@ -7,3 +7,28 @@
  1261  node app.js
 
 # 
+
+ 1224  heroku login
+ 1225  heroku create nodejs-form-submission
+ 1226  git:remote -a nodejs-form-submission
+ 1227  heroku git:remote -a nodejs-form-submission
+ 1228  git push heroku master
+ 1229  git fetch
+ 1230  git merge
+ 1231  git push heroku master
+ 1232  git:remote -a nodejs-form-submission
+ 1233  heroku git:remote -a nodejs-form-submission
+ 1234  git push heroku master
+ 1235  git status
+ 1236  git push heroku master --force
+ 1237  git branch
+ 1238  git push heroku main --force
+ 1239  heroku open
+
+
+  1213  git add .\ngit commit -m "Updated port configuration for Heroku"\ngit push heroku master\n
+ 1214  git add .\ngit commit -m "Updated port configuration for Heroku"\ngit push heroku masin
+ 1215  git push
+ 1216  ls
+ 1217  git push heroku master
+ 1218  git push heroku main
